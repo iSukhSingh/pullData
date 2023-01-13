e75896319ad23031189e9eaaa6205bd9f3ee8da0
Sukh Singh <69435459+iSukhSingh@users.noreply.github.com>
2023-01-13 13:12:45 -0500

    Add files via upload

---
+++ WebForm1.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ASPLogin.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            USER:<asp:TextBox ID="txtUser" runat="server"></asp:TextBox>
+        </div>
+        <p>
+            <asp:Label ID="message" runat="server" Text=""></asp:Label>
+        </p>
+        <p>
+            &nbsp;</p>
+        <p>
+        <asp:Button ID="Button1" runat="server" Text="login" Height="27px" Width="120px" style="margin-top: 0px" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASPLogin
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        SqlConnection SQLConn = new SqlConnection("Data Source=NDA2-SQL1\\NDASHAFTDB;Initial Catalog=NDA_TRACE_ADMIN_DB;User ID=sa;Password=NDA_Admin;");
+        //SqlConnection SQLConn = new SqlConnection("Data Source=NDA2-SQL1\\NDASHAFTDB;Initial Catalog=NDA_TRACE_ADMIN_DB; Integrated Security=True");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            try
+            {
+                // Open the connection
+                SQLConn.Open();
+
+                // Check the connection status
+                if (SQLConn.State == ConnectionState.Open)
+                {
+                    Console.WriteLine("Connection Successful");
+                    // Execute your SQL commands here
+                }
+                else
+                {
+                    Console.WriteLine("Connection Failed");
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            message.Text = "";
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter("SELECT * FROM MST_USER WHERE USER_NO='"+ txtUser.Text+"'", SQLConn);
+            DataTable DT = new DataTable();
+            sqlDataAdapter.Fill(DT);
+
+            if(DT.Rows.Count > 0)
+            {
+                message.Visible = true;
+                message.Text = "You have logged in";
+                message.ForeColor = System.Drawing.Color.Green;
+            }
+            else
+            {
+                message.Visible = true;
+                message.Text = "There was a promblem";
+                message.ForeColor = System.Drawing.Color.Red;
+            }
+
+        }
+    }
+}
